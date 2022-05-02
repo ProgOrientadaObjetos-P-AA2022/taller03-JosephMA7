04cbfe66b650fef2e6f4e694365080bd17fcbabe
JosephMA7 <jjmartinez10@utpl.edu.ec>
2022-05-02 01:38:38 -0500

    Resolucion del Taller3

--- problema1/Terreno1/src/terreno1/Terreno1.java
+++ problema1/Terreno1/src/terreno1/Terreno1.java
@@ -1,21 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package terreno1;
 
-/**
- *
- * @author Usuario
- */
+
 public class Terreno1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+     
+    private double costo_terreno;
+    private double ancho;
+    private double largo;
+    private double area;
+    private double valorMetroCuadrado;
+    
+    public void establecerAncho(double c){
+        ancho = c;
     }
     
+    public void establecerLargo(double c){
+        largo = c;
+    }
+    public void establecerValorMetroCuadrado(double c){
+        valorMetroCuadrado = c ;
+    }
+    public void calcularArea(){
+        area = ancho * largo;
+    }
+    public void calcularCostoTerreno(){
+         costo_terreno = area * valorMetroCuadrado;
+    }
+    //obtener
+    public double obtenerCostoTerreno(){
+        return  costo_terreno;
+    }
+    public double obtenerAncho(){
+        return  ancho;
+    }
+    public double obtenerLargo(){
+        return  largo;
+    }
+    public double obtenerArea(){
+        return  area;
+    }
+    public double obtenerValorMetroCuadrado(){
+        return  valorMetroCuadrado;
+    }
+
+   
 }
+    
+   
+    
+    
+
